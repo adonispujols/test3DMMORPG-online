880d868f8e26fb2cbf734b23992717e514f41a69
adonispujols <adonispujols@users.noreply.github.com>
2019-06-12 12:59:05 -0400

    added new custom id system

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-var userIDCount = 0;
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    io.emit('userConnected', userIDCount);
-    userIDCount += 1;
+io.on('connection', function(socket) {
+    socket.on('sentID', function(userID) {
+        io.emit('userJoined', userID);
+    }
 
-    socket.on('onKeyDown', function(data){
+    socket.on('onKeyDown', function(data) {
         var translateParams = {x: 0, y: 0, z: 0, distance: 0}
         switch (data.key) {
             case "left":
